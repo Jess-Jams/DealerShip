51706a306b9f0215d38573c2e03f47df98ce9b00
Jess-Jams <jchandler971@gmail.com>
2023-04-29 22:36:19 -0400

    affichage JS + responsive

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let carNames = document.getElementsByClassName("carNames")
+
+
+if (carNames.innerHTML = "Lamborghini Urus") {
+    console.log(carNames.innerHTML);
+    carNames.innerHTML = "petite voiture";
+    console.log(carNames.innerHTML);
+} else {
+    carNames.innerHTML = "voiture chere";
+}
+console.log(carNames.innerHTML + " 2");
+
+let i = 0;
+while (i <6 ) {
+    i++
+    console.log(i)
+    // if (i = 1) {
+        
+    // }
+}
