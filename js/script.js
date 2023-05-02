c7ecb892ee7df2112e60ccbcaba7d51090820d1a
Jess-Jams <jchandler971@gmail.com>
2023-05-02 14:17:19 -0400

    boucle JS

--- js/script.js
+++ js/script.js
@@ -1,20 +1,22 @@
-let carNames = document.getElementsByClassName("carNames")
-
-
-if (carNames.innerHTML = "Lamborghini Urus") {
-    console.log(carNames.innerHTML);
-    carNames.innerHTML = "petite voiture";
-    console.log(carNames.innerHTML);
-} else {
-    carNames.innerHTML = "voiture chere";
-}
-console.log(carNames.innerHTML + " 2");
-
-let i = 0;
-while (i <6 ) {
-    i++
-    console.log(i)
-    // if (i = 1) {
-        
-    // }
+let parentDiv = document.querySelector(".section");
+for (let i = 0; i < 6; i++) {
+  var newDiv = document.createElement("div");
+  newDiv.className = "row m-3 p-0 bg-light d-flex";
+  newDiv.innerHTML =`
+  <div class="col-lg-4">
+    <img class="img-fluid" src="images/products/1.jpg" alt="image Lamborghini Urus" />
+  </div>
+  <div class="col-8">
+    <h2 class="carNames text-uppercase">Lamborghini Urus</h2>
+    <p class="text-dark">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit eligendi expedita minima exercitationem accusamus reiciendis, fugiat unde sapiente soluta quaerat nostrum quasi non magnam nisi sint veniam natus nulla eius.
+    </p>
+        <div class="row">
+            <div class="col">price 79 000</div>
+            <div class="col">
+                <input type="button" value="+ comparer" class="btn btn-danger" />
+            </div>
+        </div>
+  </div>
+  `
+  parentDiv.appendChild(newDiv);
 }
